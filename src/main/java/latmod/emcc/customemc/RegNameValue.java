126113d6071d927ce04e2c3df7c86ed2e1f0acd3
LatvianModder <latvianmodder@gmail.com>
2014-10-17 21:13:15 +0300

    Removed CustomEMC, since EE3 adds that

--- src/main/java/latmod/emcc/customemc/RegNameValue.java
+++
@@ -1,9 +0,0 @@
-package latmod.emcc.customemc;
-import com.google.gson.annotations.Expose;
-
-public class RegNameValue
-{
-	@Expose public String name;
-	@Expose public Integer damage;
-	@Expose public Float value;
-}
