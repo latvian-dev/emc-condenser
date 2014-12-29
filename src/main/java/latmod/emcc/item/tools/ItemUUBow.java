02ed79b64bee1749e55b71b16ac1e5edf3596757
LatvianModder <latvianmodder@gmail.com>
2014-12-29 18:11:26 +0200

    Bow improvements

--- src/main/java/latmod/emcc/item/tools/ItemUUBow.java
+++ src/main/java/latmod/emcc/item/tools/ItemUUBow.java
@@ -2,16 +2,22 @@ package latmod.emcc.item.tools;
 import latmod.emcc.*;
 import latmod.emcc.api.ToolInfusion;
 import net.minecraft.block.Block;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.projectile.EntityArrow;
 import net.minecraft.init.Items;
 import net.minecraft.item.*;
+import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.*;
+import cpw.mods.fml.relauncher.*;
 
 public class ItemUUBow extends ItemToolEMCC // ItemBow
 {
+	@SideOnly(Side.CLIENT)
+	public IIcon[] pullIcons;
+	
 	public ItemUUBow(String s)
 	{
 		super(s);
@@ -63,7 +69,7 @@ public class ItemUUBow extends ItemToolEMCC // ItemBow
 			
 			if (hasInfusion(is, ToolInfusion.FIRE)) ea.setFire(100);
 			
-			damageItem(is, false);
+			if(!ep.capabilities.isCreativeMode) damageItem(is, false);
 			w.playSoundAtEntity(ep, "random.bow", 1F, 1F / (itemRand.nextFloat() * 0.4F + 1.2F) + f * 0.5F);
 			
 			if (flag) ea.canBePickedUp = 2;
@@ -92,5 +98,23 @@ public class ItemUUBow extends ItemToolEMCC // ItemBow
 		{ if(damageItem(is, true)) ep.setItemInUse(is, getMaxItemUseDuration(is)); }
 		
 		return is;
+	}
+	
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IIconRegister ir)
+	{
+		itemIcon = ir.registerIcon(mod.assets + "tools/bow_0");
+		
+		pullIcons = new IIcon[3];
+		for(int i = 0; i < pullIcons.length; i++)
+			pullIcons[i] = ir.registerIcon(mod.assets + "tools/bow_" + (i + 1));
+	}
+	
+	@SideOnly(Side.CLIENT)
+	public IIcon getIcon(ItemStack is, int r, EntityPlayer ep, ItemStack is1, int t)
+	{
+		if(t == 0) return itemIcon;
+		int i = (getMaxItemUseDuration(is1) - t) / 8;
+		return pullIcons[Math.min(pullIcons.length - 1, i)];
 	}
 }
