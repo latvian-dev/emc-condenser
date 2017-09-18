a2b2592d59154ebe1c7acafc41e9f2b2f138616e
LatvianModder <latvianmodder@gmail.com>
2017-09-18 03:25:44 +0300

    Added argument classes to LangKey, bugfixes

--- src/main/java/com/latmod/emc_condenser/EMCCLang.java
+++ src/main/java/com/latmod/emc_condenser/EMCCLang.java
@@ -6,11 +6,11 @@ import com.feed_the_beast.ftbl.lib.LangKey;
 /**
  * @author LatvianModder
  */
-public class EMCCLang
+public interface EMCCLang
 {
-	public static final LangKey NOTARGET = LangKey.of("lang.emc_condenser.notarget");
-	public static final LangKey EMC = LangKey.of("lang.emc_condenser.emc");
-	public static final LangKey TOTAL_EMC = LangKey.of("lang.emc_condenser.total_emc");
-	public static final LangKey SAFEMODE = LangKey.of("lang.emc_condenser.safemode");
-	public static final LangKey TAKEITEMS = LangKey.of("lang.emc_condenser.takeitems");
+	LangKey NOTARGET = LangKey.of("lang.emc_condenser.notarget");
+	LangKey EMC = LangKey.of("lang.emc_condenser.emc", String.class);
+	LangKey TOTAL_EMC = LangKey.of("lang.emc_condenser.total_emc", String.class);
+	LangKey SAFEMODE = LangKey.of("lang.emc_condenser.safemode");
+	LangKey TAKEITEMS = LangKey.of("lang.emc_condenser.takeitems");
 }
